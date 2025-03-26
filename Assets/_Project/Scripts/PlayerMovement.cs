1e41b224abc195b2697271120d03017e1a56faf2
BimmerHex <bimmerhex@gmail.com>
2025-03-26 23:56:44 +0300

    Temel hareket sistemi eklendi. Look actions için Mouse Position eklendi.

---
+++ Assets/_Project/Scripts/PlayerMovement.cs
@@ -0,0 +1,86 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private InputSystem_Actions playerActions;
+    private CharacterController characterController;
+
+    [Header("Hareket Bilgisi")]
+    [SerializeField] private float walkSpeed;
+    [SerializeField] private float runSpeed;
+    private float speed;
+    private Vector3 movementDirection;
+
+    [Header("Aim Bilgisi")]
+    [SerializeField] private Transform aim;
+    [SerializeField] private LayerMask aimLayerMask;
+    private Vector3 lookingDirection;
+
+    private Vector2 moveInput;
+    private Vector2 aimInput;
+
+    private void Awake()
+    {
+        AssignInputEvents();
+    }
+
+    private void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+
+        speed = walkSpeed;
+    }
+
+    private void Update()
+    {
+        ApplyMovement();
+        AimTowardsMouse();
+    }
+
+    private void AimTowardsMouse()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(aimInput);
+
+        if (Physics.Raycast(ray, out var hitInfo, Mathf.Infinity, aimLayerMask))
+        {
+            lookingDirection = hitInfo.point - transform.position;
+            lookingDirection.y = 0f;
+            lookingDirection.Normalize();
+
+            transform.forward = lookingDirection;
+
+            aim.position = new Vector3(hitInfo.point.x, transform.position.y, hitInfo.point.z);
+        }
+    }
+
+    private void ApplyMovement()
+    {
+        movementDirection = new Vector3(moveInput.x, 0f, moveInput.y);
+
+        if (movementDirection.magnitude > 0)
+        {
+            characterController.Move(movementDirection * Time.deltaTime * speed);
+        }
+    }
+
+    private void AssignInputEvents()
+    {
+        playerActions = new InputSystem_Actions();
+
+        playerActions.Player.Move.performed += context => moveInput = context.ReadValue<Vector2>();
+        playerActions.Player.Move.canceled += context => moveInput = Vector2.zero;
+
+        playerActions.Player.Look.performed += context => aimInput = context.ReadValue<Vector2>();
+        playerActions.Player.Look.canceled += context => aimInput = Vector2.zero;
+    }
+
+    private void OnEnable()
+    {
+        playerActions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerActions.Disable();
+    }
+}
